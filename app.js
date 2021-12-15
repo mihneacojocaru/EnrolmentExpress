77cd512c33e950ff64bc44a6f8afafbc1e81660d
mihneacojocaru <mihnea.co@gmail.com>
2021-12-15 12:49:01 +0100

    crud fix

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { getMaterials, newMaterialsList, deleteMaterial, updateMaterial } from './repository.js';
 
 const app = express();
 
@@ -9,8 +10,45 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cors());
 
-app.get('/', (req,res)=>{
-    res.status(200).json("Merge treaba!");
+app.get('/', async (req,res)=>{
+    try {
+        let materials = await getMaterials();
+        res.status(200).json(materials);
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
 });
+
+app.post('/postMaterials', (req,res)=>{
+
+    try {
+        let newMaterial = req.body;
+        newMaterialsList(newMaterial);
+        res.status(200).json("Material succesfully added");
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+
+});
+
+app.delete('/deleteItem/:id', (req,res) =>{
+    try {
+        let {id} = req.params;
+        deleteMaterial(id);
+        res.status(200).json('Deleted succesfuly');
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+});
+
+app.put('/updateItem', (req,res)=>{
+    try {
+        let item = req.body;
+        updateMaterial(item);
+        res.status(200).json('Updated successfuly');
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+})
 
 app.listen(port, ()=>console.log("Listenting on port " + port));
