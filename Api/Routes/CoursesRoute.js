325a2d029098537dfa6c08cac53aa30fec15df00
mihneacojocaru <mihnea.co@gmail.com>
2021-12-22 16:20:12 +0100

    courses route

--- Api/Routes/CoursesRoute.js
+++ Api/Routes/CoursesRoute.js
@@ -13,4 +13,33 @@ coursesRoute.get('/', async (req,res)=>{
     }
 });
 
+coursesRoute.post('/', (req,res)=>{
+    try {
+        let item = req.body;
+        //courseRepo.newCourseList(item);
+        res.status(200).json("New course sucessfuly added");
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+});
+
+coursesRoute.delete('/:studentId/:courseId', (req,res) =>{
+    try {
+        //let {id} = {... req.params};
+        //courseRepo.deleteEnrolment(req.params.studentId,req.params.courseId);
+        res.status(200).json('Deleted successfuly');
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+});
+
+coursesRoute.put('/:studentId/:courseId', (req,res)=>{
+    try {
+        //courseRepo.updateEnrolmentsList(req.body,req.params.studentId,req.params.courseId);
+        res.status(200).json("You've been succesfully enroled");
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+});
+
 export default coursesRoute;
