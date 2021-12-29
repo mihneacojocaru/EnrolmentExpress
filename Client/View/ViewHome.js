576fd63b29fb25fc9447dda814d77f0bbb0ed112
mihneacojocaru <mihnea.co@gmail.com>
2021-12-29 20:03:11 +0100

    sass

--- Client/View/ViewHome.js
+++ Client/View/ViewHome.js
@@ -1,6 +1,39 @@
 export default class ViewHome{
     constructor () {
         this.root = document.getElementById('root');
-        this.root.innerHTML = `<h1>TEST</h1>`;
+        this.root.innerHTML += this.nav();
+        this.root.innerHTML += this.main();
+    }
+
+    nav = () => {
+        return `<nav>
+                    <h1 class="homeLink">Courses</h1>
+                    <div class="elements">
+                        <span>Sign Up</span>
+                        <span>Sign In</span>
+                    </div>
+                </nav>`;
+    }
+
+    main = () => {
+        return `<main>
+                    <div class="cards-container">
+                        <div class="card">
+                            <span>Course</span>
+                            <h2>Build a Basic Bookcase</h2>
+                        </div>
+                        <div class="card">
+                            <span>Course</span>
+                            <h2>Learn How to Program</h2>
+                        </div>
+                        <div class="card">
+                            <span>Course</span>
+                            <h2>Learn How to Test Programs</h2>
+                        </div>
+                        <div class="newCourse">
+                            <h2>+ New Course</h2>
+                        </div>
+                    </div>
+                </main>`;
     }
 }
